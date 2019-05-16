e74082654a18b593e2f1c9088b46b1286a1ec896
杜金锋 <dujinfeng@meituan.com>
2019-05-16 11:46:33 +0800

    Note: 更新规范， 增加pub.dev 平分

--- packages/djf_first_lugin/lib/djf_first_lugin.dart
+++ packages/djf_first_lugin/lib/djf_first_lugin.dart
@@ -4,14 +4,22 @@ import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 
 class DjfFirstLugin {
-  static const MethodChannel _channel =
-      const MethodChannel('djf_first_lugin');
+  static const MethodChannel _channel = const MethodChannel('djf_first_lugin');
 
+  /// Returns a  string for phone system version.
+  ///
+  /// Properties get
+  ///
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  ///Returns a  string, append [surname] param conteng
+  ///
+  /// The [surname] argument string.
+  /// Is a method.
+  ///
   static Future<String> name({
     @required String surname,
   }) async {
